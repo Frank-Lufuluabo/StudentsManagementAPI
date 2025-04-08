5650aad8596b6141edc1bf6d859d3d28cc520458
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-04-09 01:39:58 +0200

    Refactor Project Structure

--- StudentManagement/Models/UserActivity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StudentManagementAPI.Models
-{
-    public class UserActivity
-    {
-        public string CreatedBy { get; set; }
-        public string CreatedOn { get; set;}
-    }
-}
